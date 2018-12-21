71629f954fd021485837e21477e23d21df8a6c0e
ChristinaLavingia <christinalavingia@gmail.com>
2018-12-20 20:56:30 -0800

    Committing API config for Google Maps and powderlin.es

--- asset/js/project1.js
+++ asset/js/project1.js
@@ -1,20 +1,31 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $.ajax({
-        url: ;
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
+    $("#search").on("click", function(event) {
+        event.preventDefault();
+
+        var userZip = $("#zip").val().trim();
+
+        $.ajax({
+            url: "https://maps.googleapis.com/maps/api/geocode/json?address="+userZip+"&key=AIzaSyCtACpMRAILtspcD0Xv5L3SADKump8sbJk",
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var maps = response.results[0];
+    
+        var lat = maps.geometry.location.lat;
+        var long = maps.geometry.location.lng;
+    
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/http://api.powderlin.es/closest_stations?lat="+lat+"&lng="+long+"&data=true&days=0&count=5",
+            method: "GET"
+          }).then(function(response) {
+            console.log(response);
+          });    
+
+        });
+    
     });
 
-    var lat = ;
-    var long = ;
-
-    $.ajax({
-        url: "http://api.powderlin.es/closest_stations?lat="+lat+"&lng="+long+"&data=true&days=0&count=5",
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-      });    
+   
 });
